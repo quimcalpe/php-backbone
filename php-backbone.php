6cf7f00d7c09dadd072d24619d9b0b05fd1f2ca6
quimcalpe <quim@kalpe.com>
2012-08-08 15:44:01 +0300

    Remove wrapping function

--- php-backbone.php
+++ php-backbone.php
@@ -1,41 +1,36 @@
 <?php
 
-function php_backbone() {
-  global $_PUT, $_DELETE, $_POST, $_GET;
-  // map global arrays, also create $_PUT and $_DELETE (unsupported by PHP)
-  $_PUT = array();
-  $_DELETE = array();
-  $method = strtoupper($_SERVER['REQUEST_METHOD']); 
-  if ( preg_match('/application\/json/i' ,$_SERVER['CONTENT_TYPE'])) {
-    // if content is JSON, parse accordingly...
-    switch ( $method ) {
-      case "GET":
-      case "POST":
-      case "PUT":
-      case "DELETE":
-        $variable = "_".$method;
-        $$variable = json_decode(file_get_contents('php://input'), true);
-        break;
-      default:
-        // unsupported method, throw error?
-    }
-  } else {
-    // Standard parsing for HTML Forms (application/x-www-form-urlencoded)
-    switch ( $method ) {
-      case "GET":
-      case "POST":
-        // nothing to do, PHP parsed normally
-        break;
-      case "PUT":
-        parse_str(file_get_contents('php://input'), $_PUT);
-        break;
-      case "DELETE":
-        parse_str(file_get_contents('php://input'), $_DELETE);
-        break;
-      default:
-        // unsupported method, throw error?
-    }
+// create $_PUT and $_DELETE globals (unsupported by PHP)
+$_PUT = array();
+$_DELETE = array();
+$method = strtoupper($_SERVER['REQUEST_METHOD']); 
+if ( preg_match('/application\/json/i' ,$_SERVER['CONTENT_TYPE'])) {
+  // if content is JSON, parse accordingly...
+  switch ( $method ) {
+    case "GET":
+    case "POST":
+    case "PUT":
+    case "DELETE":
+      $variable = "_".$method;
+      $$variable = json_decode(file_get_contents('php://input'), true);
+      break;
+    default:
+      // unsupported method, throw error?
+  }
+} else {
+  // Standard parsing for HTML Forms (application/x-www-form-urlencoded)
+  switch ( $method ) {
+    case "GET":
+    case "POST":
+      // nothing to do, PHP parsed normally
+      break;
+    case "PUT":
+      parse_str(file_get_contents('php://input'), $_PUT);
+      break;
+    case "DELETE":
+      parse_str(file_get_contents('php://input'), $_DELETE);
+      break;
+    default:
+      // unsupported method, throw error?
   }
 }
-
-php_backbone();
